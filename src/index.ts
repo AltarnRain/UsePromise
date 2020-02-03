ca5e6dc2014f3faae9258096e85efbe14544b1ce
oi <onno.invernizzi@ivention.nl>
2020-02-03 20:26:26 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+
+
+console.log("Reload 1");
+
+console.log("Reload 3");
+
+console.log("Reload 4");
+
+console.log("Reload 5");
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./dist/index.js",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    }, 
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        hot: true
+    }
+};
