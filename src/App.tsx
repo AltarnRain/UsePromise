8ea2fcabe6312e5f77e439782d7afa1d3897bfb9
oi <onno.invernizzi@ivention.nl>
2020-05-31 22:53:04 +0200

    Hello world :)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ * @preserve Copyright 2019-2020 Onno Invernizzi.
+ * This source code is subject to terms and conditions.
+ * See LICENSE.MD.
+ */
+
+import React from "react";
+
+/**
+ * Module:          Hello world!
+ * Responsibility:  Sets up a basic app functional component.
+ */
+
+export default function App(): JSX.Element {
+    return (
+        <div>Hello world</div>
+    );
+}
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * @preserve Copyright 2019-2020 Onno Invernizzi.
+ * This source code is subject to terms and conditions.
+ * See LICENSE.MD.
+ */
+
+import App from "./App";
+import ReactDOM from "react-dom";
+import React from "react";
+
+/**
+ * Module:          index
+ * Responsibility:  Application entry point.
+ */
+
+ReactDOM.render(<App />, document.getElementById("root"));
